71a8512158acb6791330e2785dbdb711ede47651
Matheussgsilva <matheussgsilva@hotmail.com>
2022-04-09 20:01:49 -0300

    Add Link with bug

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,24 @@
 import * as C from './styles'
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import linkedinIcon from '../../images/linkedin.png';
 import facebokIcon from '../../images/facebook.png';
 import discordIcon from '../../images/discord.png';
 import logo from '../../images/Logo.png';
 
-export const Header = ({ display, onEnter }) => {
+export const Header = ({ display }) => {
 
-    const [name, setName] = useState('')
+    const [name, setName] = useState('');
 
     const handleKeyUp = (e) => {
-        if(e.code === 'Enter'  && name !== '') {
-            onEnter(name);
+        if(e.code === 'Enter' && name !== '') {
+            //onEnter(name);
+            <Link to={`/search/${name}`}></Link>
         }
-    }
+    };
+
+    console.log('Nome pesquisado: ', name)
 
     return (
         <C.Header>
