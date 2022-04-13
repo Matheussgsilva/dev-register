d89a26f12300ed4e52e7762ebebd4d9ec1953abe
Matheussgsilva <matheussgsilva@hotmail.com>
2022-04-13 20:04:31 -0300

    Delete Page3

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { Page1 } from './pages/Page1';
 import { Page2 } from './pages/Page2';
-import { Page3 } from './pages/Page3';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Page1 />} />
-        <Route path='/step2' element={<Page2 />} />
-        <Route path='/search/:name' element={<Page3 />} />
+        <Route path='/devs' element={<Page2 />} />
       </Routes>
     </BrowserRouter>
   );
